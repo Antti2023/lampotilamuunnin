6caa24308da62d8995d3d13f139f8085cf1d3d7b
Tuomas Puro <tuomas.puro@esedu.fi>
2023-10-20 09:57:20 +0300

    hiukan jeesiä

--- lampotilamuunnin.js
+++ lampotilamuunnin.js
@@ -20,3 +20,16 @@ function muunnaktoc(K1) {
 function muunnactok(C1) {
     return C + 273,15
 }
+
+function muunnaLampotila(){
+
+    // hae käyttäjän syöttämä lämpötila
+    const lampotila = document.getElementById("lampotila-input").value;
+    console.log("Lämpölila-input", lampotila);
+
+    // hae käyttäjän valitsema muunnos
+    const muunnos = document.getElementById("muunnos").value;
+    console.log("Muunnos:", muunnos);
+
+    // kutsu oikeaa muunnosfunktiota
+}
